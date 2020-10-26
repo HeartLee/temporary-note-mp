40b32bc8cbb7ccea6e3ab7fd2d4b6340e89af085
Jose Lee <lg282923166@gmail.com>
2020-10-27 00:08:38 +0800

    cchore: first blood

---
+++ miniprogram/pages/info/info.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5mby5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluZm8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbIiJdfQ==
